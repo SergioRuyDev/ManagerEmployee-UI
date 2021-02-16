a2bc5cd5ebb60abc1407d8d7d80d32afae74e711
SergioRuyDev <sergioruyenator@gmail.com>
2021-02-16 17:19:47 +0400

    Refactor the service.

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,8 +1,7 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Employee} from './employee';
-
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
