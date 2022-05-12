0024db267a2e78d3679bda914a05b8c9f877baf3
SergioRuyDev <sergioruyenator@gmail.com>
2022-05-12 11:02:04 -0300

    Just for deploy.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://manageemployees-env.eba-gzpmycgk.us-east-2.elasticbeanstalk.com'
+  apiBaseUrl: 'https://ruy-manager-employee.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiBaseUrl: 'http://localhost:8080'
-  apiBaseUrl: 'http://manageemployees-env.eba-gzpmycgk.us-east-2.elasticbeanstalk.com'
+  apiBaseUrl: 'https://ruy-manager-employee.herokuapp.com'
 };
 
 /*
